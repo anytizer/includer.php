e3ace2d50f268b083f58c16b21879cef9fa7f387
server <server@example.com>
2018-03-26 12:50:34 -0600

    call deal

--- tests.local/test.php
+++ tests.local/test.php
@@ -14,11 +14,13 @@ spl_autoload_register(array(new includer(__CLASSES__), "psr4"));
  * examples/deep/class.inner.inc.php
  */
 $di = new \examples\deep\inner();
+$di->deal();
 
 /**
  * psr4/walk.php
  */
 $ps = new \psr4\walk();
+$ps->deal();
 
 print_r($di);
 print_r($ps);
